d0f03e78aab1a117761fb7bd7fe4fc11c1b1bc70
zarakikenpashi <sadiarog.abel@gmail.com>
2024-06-09 16:18:35 +0100

    Auth component UI

--- src/router.tsx
+++ src/router.tsx
@@ -2,21 +2,35 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import AuthLayout from "./layouts/AuthLayout";
+import DashboardLayout from "./layouts/DashboardLayout";
 
 
 const router = createBrowserRouter(
     [
         {
             path: "/",
-            element: <App />,
+            element: <DashboardLayout />,
+            children: [
+                {
+                    path: "/",
+                    element: <App />,
+                },
+            ],
         },
         {
-            path: "/login",
-            element: <Login />,
-        },
-        {
-            path: "/register",
-            element: <Register />,
+            path: "/auth",
+            element: <AuthLayout />,
+            children: [
+                {
+                    path: "/auth/login",
+                    element: <Login />,
+                },
+                {
+                    path: "/auth/register",
+                    element: <Register />,
+                },
+            ],
         },  
     ],
 );
